d4a27af549d96ba267fdacfb8423fecfe5ee7942
Guzz <gustavo.bragada@hotmail.com>
2025-02-07 21:40:58 -0300

    Clean up code formatting and remove unnecessary comments

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/* Define Computer's choice in the game
-Create the computerChoice function
-Assign a random integer between 1, 2 and 3 to variable randomChoice (each number represents a choice)
-If it's 1, then return 'rock' as the choice;
-Otherwise, in case it's 2, then return 'paper' as the choice;
-But in the event it isn't even 2, then return 'scissors' as the choice
-*/
-
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
     if (randomChoice == 1) {
@@ -16,14 +8,6 @@ function getComputerChoice() {
         return 'scissors'
     }
 }
-
-/* Get user's choice in the game 
-Create the getPlayerChoice function
-Prompt the user to input their choice from three options ('rock', 'paper', 'scissors')
-Store the text input to playerChoice
-Make playerChoice spring lowercase so the check is case-insensitive
-return playerChoice
-*/
 
 function getPlayerChoice() {
     let playerChoice = prompt("Welcome to the Rock, Paper and Scissors game! You'll be playing against the computer.\nChoose between 'rock', 'paper' or 'scissors'.\nPlease type the options exactly as they're written.\nRefresh page if it breaks :3");
@@ -44,6 +28,8 @@ function playGame() {
         let playerChoice = getPlayerChoice();
         console.log("You played: " + playerChoice);
         console.log("Computer played: " + computerChoice);
+
+        //This is terrible but I don't know how to do it better without arrays
         if (computerChoice == 'rock' && playerChoice == 'paper') { console.log('You win! Paper beats rock.'); playerScore++ }
         else if (computerChoice == 'paper' && playerChoice == 'rock') { console.log('You lose! Paper beats rock.'); computerScore++ }
         else if (computerChoice == 'scissors' && playerChoice == 'rock') { console.log('You win! Rock beats scissors.'); playerScore++ }
@@ -51,15 +37,11 @@ function playGame() {
         else if (computerChoice == 'paper' && playerChoice == 'scissors') { console.log('You win! Scissors beats paper.'); playerScore++ }
         else if (computerChoice == 'scissors' && playerChoice == 'paper') { console.log('You lose! Scissors beats paper.'); computerScore++ }
         else if (computerChoice === playerChoice) { console.log("It's a tie!") }
-
-
     }
 
     for (let i = 0; i <= 5; i++) {
-
         console.log("------Round " + i + "-------");
         playRound();
-
     }
 
     if (playerScore > computerScore) {
